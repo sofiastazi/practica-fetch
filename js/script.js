a46ac39edc6542408c99cbdc32fb5fca9b8dc74b
Sofia <sofia.stazi04@gmail.com>
2024-08-12 14:55:00 -0300

    Modificación a script.js para que la página muestre nombre y apellido de los estudiantes que están en el archivo data.json

--- js/script.js
+++ js/script.js
@@ -12,10 +12,22 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  */
 function showData(dataArray) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+
+  // Aquí modifiqué el codigo para que itere en los estudiantes del dataArray, ya que de otra manera me daba error
+  for (const item of dataArray.students) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+// Hago fetch para obtener los datos del json
+fetch(DATA_URL)
+  // Convierto la respuesta a un json válido
+  .then(respuesta => respuesta.json())
+  // Uso la función showData pasándole como argumento la respuesta del fetch
+  .then(respuesta => {
+    showData(respuesta);
+  }
+);
